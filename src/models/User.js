a68c55f57731f777cb4858f5a10143ed3496d3cd
Gabriel S. Gama <77582397+gabrielbrabo@users.noreply.github.com>
2023-04-05 19:46:29 -0300

    add avatar models

--- src/models/User.js
+++ src/models/User.js
@@ -20,7 +20,6 @@ const userschema = new mongoose.Schema(
         },
         avatar: {
             type: String,
-            required: true,
         },
         status: {
             type: String,
